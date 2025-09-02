9e0d87b0fc7fc6d0382ea53579eabfbbe81473fb
maxveldeman1 <max.veldeman@hotmail.com>
2025-09-02 10:34:04 +0200

    Test added

--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -1,10 +1,13 @@
 package be.vdab.goededoel;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.BIG_DECIMAL;
 
 import be.vdab.goededoel.GoedeDoel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 class GoedeDoelTest {
 	private static final String NAAM = "CliniClowns";
@@ -33,6 +36,11 @@ class GoedeDoelTest {
     @Test
     void doelenMetEenVerschillendeNaamZijnNietGelijk(){
         assertThat(doel).isEqualTo(new GoedeDoel("WWF"));
+    }
+
+    @Test
+    void doelVerschiltVanEenObjectMetEenAnderType(){
+        assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
     }
 
 }
